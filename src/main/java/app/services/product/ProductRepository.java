79878a6cfca1619d8aab23a9f9cf17ecaa1054e6
ernahulaj <erna.hulaj@gmail.com>
2023-03-31 22:05:53 +0200

    added Product CRUD and ManufacturerException

--- src/main/java/app/services/product/ProductRepository.java
+++ src/main/java/app/services/product/ProductRepository.java
@@ -1,4 +1,66 @@
 package app.services.product;
 
+import app.mongodb.MongoUtils;
+import app.mongodb.MongoCollectionWrapper;
+import app.mongodb.MongoCollections;
+import app.services.manufacturer.models.ManufacturerReference;
+import app.services.product.models.Product;
+import app.services.product.models.ProductReference;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.UpdateOneModel;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.UpdateResult;
+import com.mongodb.reactivestreams.client.ClientSession;
+import io.quarkus.mongodb.reactive.ReactiveMongoCollection;
+import io.smallrye.mutiny.Uni;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ApplicationScoped
 public class ProductRepository {
+    @Inject
+    MongoCollectionWrapper mongoCollectionWrapper;
+
+    public ReactiveMongoCollection<Product> getCollection() {
+        return mongoCollectionWrapper.getCollection(MongoCollections.PRODUCTS_COLLECTION, Product.class);
+    }
+
+    public Uni<List<Product>> getListByManufacturerId(String id) {
+        return getCollection().find(Filters.eq(Product.FIELD_MANUFACTURER_ID, id)).collect().asList();
+    }
+
+    public Uni<Product> getById(String id) {
+        return getCollection().find(Filters.eq(Product.FIELD_ID, id)).toUni();
+    }
+
+    public Uni<Product> add(Product product) {
+        return MongoUtils.add(getCollection(), product);
+    }
+
+    public Uni<Product> update(String id, Product product) {
+        return MongoUtils.update(getCollection(), Filters.eq(Product.FIELD_ID, id), product);
+    }
+
+    public Uni<UpdateResult> updateManufactures(ManufacturerReference manufacturerReference) {
+        return getCollection().updateMany(Filters.eq(Product.FIELD_MANUFACTURER_ID, manufacturerReference.getId()), Updates.set(Product.FIELD_MANUFACTURER, manufacturerReference));
+    }
+
+    public Uni<Void> updateStockQuantity(ClientSession session, boolean sign, List<ProductReference> productReferences) {
+        List<UpdateOneModel<Product>> updates;
+        if (sign) {
+            updates = productReferences.stream().map(productReference -> new UpdateOneModel<Product>(Filters.eq(Product.FIELD_ID, productReference._id),
+                    Updates.inc(Product.FIELD_STOCK_QUANTITY, productReference.getQuantity()))).collect(Collectors.toList());
+        } else {
+            updates = productReferences.stream().map(productReference -> new UpdateOneModel<Product>(Filters.eq(Product.FIELD_ID, productReference._id),
+                    Updates.inc(Product.FIELD_STOCK_QUANTITY, -productReference.getQuantity()))).collect(Collectors.toList());
+        }
+        return getCollection().bulkWrite(session, updates).replaceWithVoid();
+    }
+
+    public Uni<Product> delete(String id) {
+        return getCollection().findOneAndDelete(Filters.eq(Product.FIELD_ID, id));
+    }
 }
--- src/main/java/app/services/product/ProductResource.java
+++ src/main/java/app/services/product/ProductResource.java
@@ -1,4 +1,47 @@
 package app.services.product;
 
+import app.helpers.PaginatedResponse;
+import app.services.product.models.CreateProduct;
+import app.services.product.models.Product;
+import app.services.product.models.UpdateProduct;
+import io.smallrye.mutiny.Uni;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/products")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class ProductResource {
+    @Inject
+    ProductService service;
+
+    @GET
+    public Uni<PaginatedResponse<Product>> getList(@BeanParam ProductFilterWrapper wrapper) {
+        return service.getList(wrapper);
+    }
+
+    @GET
+    @Path("/{id}")
+    public Uni<Product> getById(@PathParam("id") String id) {
+        return service.getById(id);
+    }
+
+    @POST
+    public Uni<Product> add(CreateProduct createProduct) {
+        return service.add(createProduct);
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Uni<Product> update(@PathParam("id") String id, UpdateProduct updateProduct) {
+        return service.update(id, updateProduct);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Uni<Void> delete(@PathParam("id") String id) {
+        return service.delete(id);
+    }
 }
