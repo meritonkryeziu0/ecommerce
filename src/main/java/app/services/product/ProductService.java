d0710ba92b9956329fbac52b9ce1a35e5862e003
ernahulaj <erna.hulaj@gmail.com>
2023-03-31 21:38:16 +0200

    added Product models

---
+++ src/main/java/app/services/product/ProductService.java
@@ -0,0 +1,4 @@
+package app.services.product;
+
+public class ProductService {
+}
---
+++ src/main/java/app/services/product/models/CreateProduct.java
@@ -0,0 +1,45 @@
+package app.services.product.models;
+
+import lombok.Data;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.util.Map;
+
+@Data
+public class CreateProduct {
+    @NotBlank
+    @Size(max = 250)
+    private String name;
+    @Size(max = 1000)
+    private String description;
+    @NotNull
+    @DecimalMin(value = "0.0")
+    private Double price;
+    @NotNull
+    @Min(value = 1)
+    private Integer stockQuantity;
+    @NotBlank
+    private String type;
+    @Valid
+    private ManufacturerReference manufacturer;
+    private Map<String, String> details;
+    @Valid
+    private PackageDetails packageDetails;
+
+    @Data
+    public static class PackageDetails {
+        @NotNull
+        @Min(value = 0)
+        private Double weight;
+        @NotNull
+        @Min(value = 0)
+        private Double height;
+        @NotNull
+        @Min(value = 0)
+        private Double width;
+        @NotNull
+        @Min(value = 0)
+        private Double depth;
+    }
+}
