5f318556096193e9fd9b215b0c7fcb49403a0962
erna-hulaj <119673098+erna-hulaj@users.noreply.github.com>
2023-04-06 17:44:54 +0200

    wishlist simple crud

--- src/main/java/app/services/wishlist/WishlistRepository.java
+++ src/main/java/app/services/wishlist/WishlistRepository.java
@@ -1,4 +1,46 @@
 package app.services.wishlist;
 
+import app.mongodb.MongoCollectionWrapper;
+import app.mongodb.MongoCollections;
+import app.mongodb.MongoUtils;
+import app.services.wishlist.models.Wishlist;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import com.mongodb.reactivestreams.client.ClientSession;
+import io.quarkus.mongodb.reactive.ReactiveMongoCollection;
+import io.smallrye.mutiny.Uni;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.ArrayList;
+
+@ApplicationScoped
 public class WishlistRepository {
+  @Inject
+  MongoCollectionWrapper mongoCollectionWrapper;
+
+  public ReactiveMongoCollection<Wishlist> getCollection() {
+    return mongoCollectionWrapper.getCollection(MongoCollections.SHOPPING_CARTS_COLLECTION, Wishlist.class);
+  }
+
+  public Uni<Wishlist> getById(String id) {
+    return getCollection().find(Filters.eq(Wishlist.FIELD_ID, id)).toUni();
+  }
+
+  public Uni<Wishlist> getByUserId(String userId) {
+    return getCollection().find(Filters.eq(Wishlist.FIELD_USER_ID, userId)).toUni();
+  }
+
+  public Uni<Wishlist> add(ClientSession session, Wishlist wishlist) {
+    return MongoUtils.addEntity(session, getCollection(), wishlist);
+  }
+
+  public Uni<Wishlist> update(String userId, Wishlist wishlist) {
+    return MongoUtils.updateEntity(getCollection(), Filters.eq(Wishlist.FIELD_USER_ID, userId), wishlist);
+  }
+
+  public Uni<Void> emptyWishlist(String userId) {
+    return getCollection().findOneAndUpdate(Filters.eq(Wishlist.FIELD_USER_ID, userId),
+        Updates.set(Wishlist.FIELD_PRODUCTS, new ArrayList<>())).replaceWithVoid();
+  }
 }
--- src/main/java/app/services/wishlist/exceptions/WishlistException.java
+++ src/main/java/app/services/wishlist/exceptions/WishlistException.java
@@ -4,6 +4,7 @@ import app.exceptions.BaseException;
 
 public class WishlistException extends BaseException {
   public static String WISHLIST_NOT_FOUND = "Wishlist not found!";
+  public static String PRODUCT_NOT_ADDED = "Product not added!";
   public WishlistException(String message, int statusCode) {
     super(message, statusCode);
   }
