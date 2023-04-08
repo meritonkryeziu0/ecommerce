1310a76a556be5c778932c4b4b920b5d297d948b
ernahulaj <erna.hulaj@gmail.com>
2023-04-08 12:13:06 +0200

    fixed CreateShoppingCart issue

---
+++ src/main/java/app/services/shoppingCart/models/CreateShoppingCart.java
@@ -0,0 +1,23 @@
+package app.services.shoppingCart.models;
+
+import app.services.accounts.models.User;
+import app.services.accounts.models.UserReference;
+import app.services.product.models.ProductReference;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Getter
+@Setter
+public class CreateShoppingCart {
+  private UserReference userReference;
+  @Valid
+  List<ProductReference> products;
+  private Double total;
+
+  public CreateShoppingCart(User user){
+    this.userReference = new UserReference(user.id, user.getFirstName(), user.getLastName());
+  }
+}
