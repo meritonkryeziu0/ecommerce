a151ac5ec8f91d5544e557171cf92908a2bf2be1
ernahulaj <erna.hulaj@gmail.com>
2023-04-08 12:03:44 +0200

    fixed product removal from wishlist

--- src/main/java/app/services/shoppingCart/ShoppingCartMapper.java
+++ src/main/java/app/services/shoppingCart/ShoppingCartMapper.java
@@ -2,7 +2,6 @@ package app.services.shoppingCart;
 
 //import app.services.order.models.CreateOrder;
 //import app.services.order.models.OrderDetails;
-import app.services.shoppingCart.models.CreateShoppingCart;
 import app.services.shoppingCart.models.ShoppingCart;
 
 import java.util.ArrayList;
