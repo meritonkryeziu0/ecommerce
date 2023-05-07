1f39375775a908bc5e9cd3757ca5ada9a71d7910
rinabekolli <rina.bekolli@student.uni-pr.edu>
2023-05-07 17:39:19 +0200

    added Tracking and UpdateOrderStatus models

---
+++ src/main/java/app/services/order/models/Tracking.java
@@ -0,0 +1,26 @@
+package app.services.order.models;
+
+import app.proto.TrackingReply;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tracking {
+  private String carrierCompany;
+  private String trackingNumber;
+  private String status;
+  private String estimatedDelivery;
+
+  public Tracking(TrackingReply trackingReply){
+    this.carrierCompany = trackingReply.getCarrierCompany();
+    this.trackingNumber = trackingReply.getTrackingNumber();
+    this.status = trackingReply.getStatus();
+    this.estimatedDelivery = trackingReply.getEstimatedDelivery();
+  }
+}
---
+++ src/main/java/app/services/order/models/UpdateOrderStatus.java
@@ -0,0 +1,17 @@
+package app.services.order.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateOrderStatus {
+  @NotBlank
+  String status;
+}
