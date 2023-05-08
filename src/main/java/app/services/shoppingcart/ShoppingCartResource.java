d082379f7910096baeb2b9d31bd38b9a7d94b5cb
erakadiri <eraakadiri@gmail.com>
2023-05-08 22:19:18 +0200

    implemented ShoppingCartResource and ShoppingCartService classes

--- src/main/java/app/services/shoppingcart/ShoppingCartResource.java
+++ src/main/java/app/services/shoppingcart/ShoppingCartResource.java
@@ -1,5 +1,9 @@
 package app.services.shoppingcart;
 
+import app.services.order.models.OrderDetails;
+import app.services.product.models.ProductReference;
+import app.services.shoppingcart.models.ShoppingCart;
+import app.shared.SuccessResponse;
 import io.smallrye.mutiny.Uni;
 
 import javax.inject.Inject;
@@ -7,7 +11,6 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/users/{userId}/shopping-cart")
-//@Path("/users")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ShoppingCartResource {
@@ -15,8 +18,23 @@ public class ShoppingCartResource {
   ShoppingCartService service;
 
   @GET
-  public Uni<Void> test(){
-    return Uni.createFrom().voidItem();
+  public Uni<ShoppingCart> getByUserId(@PathParam("userId") String userId) {
+    return service.getByUserId(userId);
   }
 
+  @PUT
+  public Uni<ShoppingCart> update(@PathParam("userId") String id, ProductReference productReference) {
+    return service.update(null, id, productReference);
+  }
+
+  @POST
+  @Path("/buy")
+  public Uni<ShoppingCart> buy(@PathParam("userId") String id, OrderDetails orderDetails) {
+    return service.buyShoppingCart(id, orderDetails);
+  }
+
+  @DELETE
+  public Uni<SuccessResponse> emptyShoppingCart(@PathParam("userId") String userId) {
+    return service.emptyShoppingCart(userId);
+  }
 }
--- src/main/java/app/services/shoppingcart/ShoppingCartService.java
+++ src/main/java/app/services/shoppingcart/ShoppingCartService.java
@@ -2,15 +2,12 @@ package app.services.shoppingcart;
 
 import app.common.CustomValidator;
 import app.exceptions.BaseException;
-import app.helpers.ProductUpdateHelper;
 import app.helpers.PaginatedResponse;
 import app.helpers.PaginationWrapper;
-import app.mongodb.MongoCollectionWrapper;
+import app.helpers.ProductUpdateHelper;
 import app.mongodb.MongoCollections;
 import app.mongodb.MongoSessionWrapper;
 import app.mongodb.MongoUtils;
-import app.helpers.PaginatedResponse;
-import app.helpers.PaginationWrapper;
 import app.services.order.OrderService;
 import app.services.order.models.CreateOrder;
 import app.services.order.models.OrderDetails;
@@ -23,7 +20,7 @@ import app.services.shoppingcart.models.ShoppingCart;
 import app.shared.SuccessResponse;
 import app.utils.Utils;
 import com.mongodb.reactivestreams.client.ClientSession;
-import io.quarkus.mongodb.reactive.ReactiveMongoCollection;
+import io.quarkus.scheduler.Scheduled;
 import io.smallrye.common.constraint.Nullable;
 import io.smallrye.mutiny.Uni;
 
@@ -40,24 +37,27 @@ public class ShoppingCartService {
 
   @Inject
   ShoppingCartRepository repository;
+
   @Inject
   ProductService productService;
 
   @Inject
   OrderService orderService;
+
   @Inject
   ProductUpdateHelper productUpdateHelper;
-  @Inject
-  MongoCollectionWrapper mongoCollectionWrapper;
+
   @Inject
   MongoSessionWrapper sessionWrapper;
 
-  public ReactiveMongoCollection<ShoppingCart> getCollection() {
-    return mongoCollectionWrapper.getCollection(MongoCollections.SHOPPING_CARTS_COLLECTION, ShoppingCart.class);
+  public Uni<ShoppingCart> getById(String id) {
+    return ShoppingCart.findById(id)
+        .onItem().ifNull().failWith(new ShoppingCartException(ShoppingCartException.SHOPPING_CART_NOT_FOUND, Response.Status.NOT_FOUND))
+        .map(Utils.mapTo(ShoppingCart.class));
   }
 
   public Uni<PaginatedResponse<ShoppingCart>> getList(PaginationWrapper wrapper) {
-    return MongoUtils.getPaginatedItems(getCollection(), wrapper);
+    return MongoUtils.getPaginatedItems(repository.getCollection(), wrapper);
   }
 
   public Uni<ShoppingCart> getByUserId(String userId) {
@@ -69,29 +69,14 @@ public class ShoppingCartService {
   public Uni<ShoppingCart> add(ClientSession session, CreateShoppingCart createShoppingCart) {
     return validator.validate(createShoppingCart)
         .replaceWith(ShoppingCartMapper.INSTANCE.from(createShoppingCart))
-        .flatMap(shoppingCart -> repository.add(session, shoppingCart));
+        .call(shoppingCart -> repository.add(session, shoppingCart));
   }
 
-  //  @Scheduled(every = "50s")
+  @Scheduled(every = "15m")
   public Uni<Void> clearNotUpdatedCarts() {
     return repository.clearNotUpdatedCarts();
   }
 
-  public Uni<ShoppingCart> update(String userId, ProductReference productReference) {
-    return validator.validate(productReference)
-        .replaceWith(productService.getById(productReference.id))
-        .onFailure().transform(transformToBadRequest())
-        .flatMap(product -> {
-          if (productReference.getQuantity() > product.getStockQuantity()) {
-            return Uni.createFrom().failure(new ProductException(ProductException.NOT_ENOUGH_STOCK, Response.Status.BAD_REQUEST));
-          }
-          return Uni.createFrom().item(product);
-        })
-        .replaceWith(this.getByUserId(userId))
-        .onFailure().transform(transformToBadRequest())
-        .map(shoppingCart -> this.updateShoppingCart(shoppingCart, productReference))
-        .call(MongoUtils::updateEntity);
-  }
 
   private ShoppingCart updateShoppingCart(ShoppingCart shoppingCart, ProductReference productReference) {
     Optional<ProductReference> optionalProductReference = shoppingCart.getProducts().stream()
@@ -104,17 +89,6 @@ public class ShoppingCartService {
     shoppingCart.setTotal(shoppingCart.getTotal() + productReference.getQuantity() * productReference.getPrice());
     return shoppingCart;
   }
-
-  public Uni<Void> updateAllCarts(ClientSession session, ProductReference product) {
-    return productUpdateHelper.updateProductOccurrences(session, MongoCollections.SHOPPING_CARTS_COLLECTION, ShoppingCart.class, product);
-  }
-
-  public Uni<SuccessResponse> emptyShoppingCart(String userId) {
-    return repository.emptyShoppingCart(userId)
-        .onFailure().transform(transformToBadRequest())
-        .replaceWith(SuccessResponse.toSuccessResponse());
-  }
-
 
   public Uni<ShoppingCart> update(@Nullable ClientSession session, String userId, ProductReference productReference) {
     return validator.validate(productReference)
@@ -134,6 +108,32 @@ public class ShoppingCartService {
             return MongoUtils.updateEntity(shoppingCart);
           }
           return repository.update(session, userId, shoppingCart);
+        });
+  }
+
+  public Uni<Void> updateAllCarts(ClientSession session, ProductReference product) {
+    return productUpdateHelper.updateProductOccurrences(session, MongoCollections.SHOPPING_CARTS_COLLECTION, ShoppingCart.class, product);
+  }
+
+  public Uni<SuccessResponse> emptyShoppingCart(String userId) {
+    return repository.emptyShoppingCart(userId)
+        .onFailure().transform(transformToBadRequest())
+        .replaceWith(SuccessResponse.toSuccessResponse());
+  }
+
+  public Uni<ShoppingCart> buyShoppingCart(String userId, OrderDetails orderDetails) {
+    return this.getById(userId).onFailure().transform(transformToBadRequest())
+        .flatMap(shoppingCart -> {
+          CreateOrder createOrder = ShoppingCartMapper.from(shoppingCart, orderDetails);
+          return Uni.createFrom().item(createOrder)
+              .flatMap(orderToCreate -> sessionWrapper.getSession()
+                  .flatMap(
+                      session -> orderService.add(session, orderToCreate)
+                          .replaceWith(this.emptyShoppingCart(userId))
+                          .replaceWith(Uni.createFrom().publisher(session.commitTransaction()))
+                          .replaceWith(shoppingCart)
+                          .eventually(session::close)
+                  ));
         });
   }
 
