cb29d55f5b527f57d592d091f36fd7c836664e91
Meriton Kryeziu <36973170+meritonkryeziu0@users.noreply.github.com>
2023-05-29 20:05:42 +0200

    add permit all on user login

--- src/main/java/app/services/auth/AuthenticationResource.java
+++ src/main/java/app/services/auth/AuthenticationResource.java
@@ -26,6 +26,7 @@ public class AuthenticationResource {
 
   @POST
   @Path("/register")
+  @PermitAll
   public Uni<User> register(RegisterUser registerUser) {
     return authenticationService.register(registerUser);
   }
